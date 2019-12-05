4645f5bf957c2b0ae6629fa2d367be2591a7b180
BradOdera <bradodera@gmail.com>
2019-12-05 14:59:45 +0300

    Add user tests

--- app/models.py
+++ app/models.py
@@ -6,3 +6,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     pass_secure = db.Column(db.String(255))
+ @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
