16e17f4f26d9ac18abbf8868e9ffcbfe5674023b
BradOdera <bradodera@gmail.com>
2019-12-05 15:15:09 +0300

    Add routes to display authentication forms

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -9,11 +10,12 @@ def load_user(user_id):
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String)
-    usernae = db.Column(db.String)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
     pass_secure = db.Column(db.String(255))
-    
+
  @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
