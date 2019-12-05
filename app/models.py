0a7c56efac8f65b9d4ab0013bea1e23bef44579a
BradOdera <bradodera@gmail.com>
2019-12-05 16:08:13 +0300

    model fuction to display ptches

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -24,6 +25,37 @@ class User(UserMixin, db.Model):
     def password(self, password):
         self.pass_secure = generate_password_hash(password)
 
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
-    def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+
+class Pitch(db.Model):
+    """docstring for Pitch class that defines the piches object. Getting pitches and single pitch"""
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    content = db.Column(db.String(1000))
+    category = db.Column(db.String)
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+
+    comments = db.relationship('Comment', backref='pitch', lazy="dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls, category):
+        pitches = Pitch.query.filter_by(category=category).all()
+
+        return pitches
+
+    @classmethod
+    def get_single_pitch(cls, id):
+        single_pitch = Pitch.query.filter_by(id=id).first()
+
+        return single_pitch
