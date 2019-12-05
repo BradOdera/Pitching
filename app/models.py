0cd39b676dca7c0e5943940a2edf80fe37ad47d4
BradOdera <bradodera@gmail.com>
2019-12-05 14:54:36 +0300

    create column table to store user passwords

--- app/models.py
+++ app/models.py
@@ -2,5 +2,6 @@ from . import login_manager
 from flask_login import UserMixin
 
 
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
+    pass_secure = db.Column(db.String(255))
