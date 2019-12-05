8a0eeed5778a8f5d04b48e728fd6791527dfbd77
BradOdera <bradodera@gmail.com>
2019-12-05 15:12:56 +0300

    Add forms for authentication

--- app/models.py
+++ app/models.py
@@ -2,18 +2,26 @@ from . import login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    full_name = db.Column(db.String)
+    usernae = db.Column(db.String)
     pass_secure = db.Column(db.String(255))
+    
  @property
-        def password(self):
-            raise AttributeError('You cannot read the password attribute')
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-        @password.setter
-        def password(self, password):
-            self.pass_secure = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
 
-        def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
